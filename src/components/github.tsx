dc07c0c382b39a761c17a36148d06970ed49c80d
biscuit_zhou <biscuit_zhou@outlook.com>
2025-10-08 09:27:45 +0800

    fix: wrap GitHub icon button with a link to the repository

--- src/components/github.tsx
+++ src/components/github.tsx
@@ -1,13 +1,16 @@
 import { GitHub } from "@mui/icons-material";
 import { IconButton, Tooltip } from "@mui/material";
 import { button } from "@/lib/classes";
+import Link from "@/components/link";
 
 const GitHubIconButton = () => {
     return (
         <Tooltip title="GitHub">
-            <IconButton color="inherit" className={button.icon}>
-                <GitHub />
-            </IconButton>
+            <Link href="https://github.com/Byte-Biscuit/block-style-cms">
+                <IconButton color="inherit" className={button.icon}>
+                    <GitHub />
+                </IconButton>
+            </Link>
         </Tooltip>
     );
 };
